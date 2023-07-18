1f5940044b5ffe7e0f56d687bab2c065f0ee8706
joker1617 <jeremybonnin@gmail.com>
2023-07-18 12:55:47 +0200

    commencement du projet

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+const form = document.querySelector("form");
+const resultContainer = document.querySelector(".resultContainer");
+
+async function fetchResearch() {
+  await fetch(
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=cat"
+  );
+  console.log(data);
+  //     .then((res) => res.json())
+  //     .then((data) => (researchData = data));
+
+  //   console.log(researchData);
+  //   researchDisplay();
+}
