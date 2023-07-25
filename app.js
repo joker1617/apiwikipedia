2f222de193a3e6a5f60113fedab0638ecf006d6e
joker1617 <jeremybonnin@gmail.com>
2023-07-25 22:50:39 +0200

    suite du projet

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
 const form = document.querySelector("form");
+const input = document.querySelector("input");
 const resultContainer = document.querySelector(".resultContainer");
 
-async function fetchResearch() {
-  await fetch(
-    "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=cat"
-  );
-  console.log(data);
-  //     .then((res) => res.json())
-  //     .then((data) => (researchData = data));
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  performSearch();
+});
 
-  //   console.log(researchData);
-  //   researchDisplay();
+async function performSearch() {
+  const searchInput = document.getElementById("searchInput").value;
+  await fetch(
+    `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      let output = "<ul>";
+      data.query.search.forEach((result) => {
+        const articleURL = `https://en.wikipedia.org/wiki/${encodeURIComponent(
+          result.title
+        )}`;
+        output += `<li><a href="${articleURL}" target="_blank">${result.title}</a></li>`;
+        output += `<p>${result.snippet}</p></li>`;
+      });
+      output += "</ul>";
+
+      resultContainer.innerHTML = output;
+    })
+    .catch((error) => {
+      console.error(
+        "Une erreur s'est produite lors de la récupération des données:",
+        error
+      );
+    });
 }
